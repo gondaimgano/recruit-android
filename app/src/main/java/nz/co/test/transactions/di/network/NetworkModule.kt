2bfdc27e5decc0a22b1fe0cb5fd187d8d5071409
Gondai Mgano <gondaimgano@gmail.com>
2024-07-17 07:52:24 +1200

    setup api url and integration with `TransactionsService`

--- app/src/main/java/nz/co/test/transactions/di/network/NetworkModule.kt
+++ app/src/main/java/nz/co/test/transactions/di/network/NetworkModule.kt
@@ -2,12 +2,17 @@ package nz.co.test.transactions.di.network
 
 import dagger.Module
 import dagger.Provides
+import nz.co.test.transactions.BuildConfig
 import nz.co.test.transactions.services.TransactionsService
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module
 class NetworkModule {
     @Provides
-    fun providesTransactionService(retrofit: Retrofit): TransactionsService =
-        retrofit.create(TransactionsService::class.java)
+    fun providesTransactionService(): TransactionsService =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build().create(TransactionsService::class.java)
 }
