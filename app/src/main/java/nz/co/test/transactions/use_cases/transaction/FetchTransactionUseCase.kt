2b8a3a3d3b2c9f23df2ce79d54a96ac7af2939b5
Gondai Mgano <gondaimgano@gmail.com>
2024-07-17 13:04:39 +1200

    minor change to the usecase

--- app/src/main/java/nz/co/test/transactions/use_cases/transaction/FetchTransactionUseCase.kt
+++ app/src/main/java/nz/co/test/transactions/use_cases/transaction/FetchTransactionUseCase.kt
@@ -3,8 +3,10 @@ package nz.co.test.transactions.use_cases.transaction
 import nz.co.test.transactions.services.Transaction
 import nz.co.test.transactions.services.TransactionsService
 import nz.co.test.transactions.use_cases.FetchUseCase
+import javax.inject.Inject
 
 class FetchTransactionUseCase
+@Inject
     constructor(private  val service: TransactionsService): FetchUseCase<List<Transaction>> {
     override suspend fun invoke(): List<Transaction> {
         return service.retrieveTransactions()
